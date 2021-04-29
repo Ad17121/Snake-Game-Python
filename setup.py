4ce09d19a2efe5d7cebbe22a6a36a64864c4bd76
Ad17121 <43483362+Ad17121@users.noreply.github.com>
2021-04-29 17:12:10 +0100

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-import cx_Freeze
-
-executables = [cx_Freeze.Executable("snake.py")]
-
-cx_Freeze.setup(
-    name = "Snake",
-    options = {"build_exe" : {"packages":["pygame"]}},
-    executables = executables
-)
